afee86696cb7ade6124ebc014c492e09ce2b6d11
slh <sl.hafemann@proton.me>
2023-03-12 21:00:02 +0100

    Add Netherite Nuggets as a Piglin Brute drop chance

--- src/main/java/xyz/hafemann/netheriteextras/event/ModEvents.java
+++ src/main/java/xyz/hafemann/netheriteextras/event/ModEvents.java
@@ -1,12 +1,17 @@
 package xyz.hafemann.netheriteextras.event;
 
 import net.fabricmc.fabric.api.entity.event.v1.ServerLivingEntityEvents;
+import net.fabricmc.fabric.api.loot.v2.LootTableEvents;
 import net.minecraft.advancement.criterion.Criteria;
+import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.damage.DamageSource;
 import net.minecraft.entity.effect.StatusEffectInstance;
 import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.item.ItemStack;
+import net.minecraft.loot.LootPool;
+import net.minecraft.loot.entry.ItemEntry;
+import net.minecraft.loot.provider.number.BinomialLootNumberProvider;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.stat.Stats;
 import net.minecraft.util.Hand;
@@ -16,6 +21,14 @@ import xyz.hafemann.netheriteextras.item.ModItems;
 public class ModEvents {
     public static void registerModEvents() {
         NetheriteExtras.LOGGER.debug("Registering Mod Events for " + NetheriteExtras.MOD_ID);
+
+        LootTableEvents.MODIFY.register((resourceManager, lootManager, id, supplier, setter) -> {
+            if (id.equals(EntityType.PIGLIN_BRUTE.getLootTableId())) {
+                supplier.pool(LootPool.builder()
+                        .rolls(BinomialLootNumberProvider.create(1, 0.5F))
+                        .with(ItemEntry.builder(ModItems.NETHERITE_NUGGET).build()).build());
+            }
+        });
 
         ServerLivingEntityEvents.ALLOW_DEATH.register((LivingEntity livingEntity, DamageSource damageSource, float damageAmount) -> {
             for (Hand hand : Hand.values()) {
